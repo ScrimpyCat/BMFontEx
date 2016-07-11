f646c92883cfb41408e2b6c47b9aa40f4d17285d
Stefan Johnson <sj@scrimpycat.io>
2016-07-11 12:04:08 +1000

    Updated version: 0.0.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BMFont.Mixfile do
         [
             app: :bmfont,
             description: "A BMFont file format parser",
-            version: "0.0.1",
+            version: "0.0.2",
             elixir: "~> 1.2",
             build_embedded: Mix.env == :prod,
             start_permanent: Mix.env == :prod,
