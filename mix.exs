18008684a59518fbcba175364e8a2679386a9e8c
Stefan Johnson <sj@scrimpycat.io>
2016-07-11 11:35:35 +1000

    Include Tonic in application dependencies

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule BMFont.Mixfile do
     #
     # Type "mix help compile.app" for more information
     def application do
-        [applications: [:logger]]
+        [applications: [:tonic, :logger]]
     end
 
     # Dependencies can be Hex packages:
